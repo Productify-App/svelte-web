ca7eba9db61f27413546ddcc4c73b4e5227df791
Trevor <65660736+Paridax@users.noreply.github.com>
2024-02-13 20:12:29 -0500

    Add animation to landing page, polishing

--- src/lib/components/FeatureCard.svelte
+++ src/lib/components/FeatureCard.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
+    import {ArrowRight, Icon} from "svelte-hero-icons";
     export let title: string;
     export let description: string;
+    export let icon: string = ArrowRight;
 </script>
 
-<div class="p-12 flex flex-col items-center bg-accent-1 border border-accent-4 rounded-xl  shadow-xl shadow-accent-2">
-    <slot name="icon" />
-    <div>
-
-    </div>
-    <h1 class="heading-lg bold mt-5 text-center w-full">{title}</h1>
-    <p class=" mt-5 text-center w-full">{description}</p>
+<div class="p-12 flex flex-col items-center bg-accent-1 border border-accent-4 rounded-xl shadow-xl shadow-accent-2">
+    <Icon src="{icon}" class="w-20 h-20 text-accent-7" />
+    <h1 class="heading-lg bold mt-5 text-center w-full text-accent-10">{title}</h1>
+    <p class=" mt-5 text-center w-full text-accent-8">{description}</p>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,35 @@
     import phonePic from "$lib/iphone.png";
     import placeholder from "$lib/placeholder.png";
     import FeatureCard from "$lib/components/FeatureCard.svelte";
+    import Saos from "saos";
+    import {DevicePhoneMobile, PaintBrush, User} from "svelte-hero-icons";
+
+    const threeCards = [
+        {
+            "title": "Make a difference",
+            "description": "It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part",
+            "icon": PaintBrush
+        },
+        {
+            "title": "Challenge yourself",
+            "description": "It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part.",
+            "icon": User
+        },
+        {
+            "title": "Join Productify",
+            "description": "It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part.",
+            "icon": DevicePhoneMobile
+        }
+    ]
+
+    console.log(threeCards);
 </script>
 
 <style>
     .blurbox {
         width: 60vw;
         height: 40vh;
-        @apply bg-accent-2;
+        @apply bg-accent-2 rounded-full;
         filter: blur(700px);
     }
 
@@ -24,6 +46,39 @@
         right: -10%;
         bottom: 50%;
         transform: translate(20%, 50%) rotate(26deg);
+    }
+
+    @keyframes -global-slide-in {
+        0% {
+            transform: translateX(5vw);
+            opacity: 0;
+        }
+        100% {
+            transform: translateX(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes -global-slide-up {
+        0% {
+            transform: translateY(2vh);
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+    }
+
+    @keyframes -global-slide-up-big {
+        0% {
+            transform: translateY(10vh);
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0);
+            opacity: 1;
+        }
     }
 </style>
 
@@ -40,7 +95,7 @@
                 </span>
                 a challenge...
             </h1>
-        <p class="text-xl text-surface-8 my-6 mr-24 font-medium">
+        <p class="text-lg text-surface-8 my-6 mr-24 font-medium">
             Welcome to Productify, a company dedicated to improving the lives of others by making productivity as easy as possible. Make a difference. Change lives. <strong>We’re hiring!</strong>
         </p>
         <div class="flex gap-4">
@@ -60,17 +115,21 @@
     <img src={placeholder} alt="iPhone" class="w-[500px] h-[600px] object-contain ml-16" />
     <div class="flex flex-col gap-4 w-1/3">
         <div class="relative">
-            <h1 class="display-md bold">What
-                <span class="display-md bold -z-10 text-primary-5 relative">
-                    do
-                    <svg class="absolute inline-block -z-10 top-[43%] left-[40%] center" width="120" height="185" viewBox="0 0 217 185" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M149.854 0.79892L216.093 184.733L0.523304 126.971L149.854 0.79892Z" fill="#E5EEFF"/>
-                    </svg>
-                </span>
-                we do?</h1>
-            <p class="mt-6">
-                Productify is a productivity app that allows users to manage their tasks through a simple interface. Productify allows friends to share their task with each other and uses positive peer pressure to encourage improvement.
-            </p>
+            <Saos once={true} animation={"slide-in 1s cubic-bezier(0.55, 0, 0.1, 1) both"}>
+                <h1 class="display-md bold">What
+                    <span class="display-md bold -z-10 text-primary-5 relative">
+                        do
+                        <svg class="absolute inline-block -z-10 top-[43%] left-[40%] center" width="120" height="185" viewBox="0 0 217 185" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M149.854 0.79892L216.093 184.733L0.523304 126.971L149.854 0.79892Z" fill="#E5EEFF"/>
+                        </svg>
+                    </span>
+                    we do?</h1>
+            </Saos>
+            <Saos once={true} animation={"slide-up 1s cubic-bezier(0.55, 0, 0.1, 1) both 0.5s"}>
+                <p class="mt-6 text-lg font-medium text-surface-8">
+                    Productify is a productivity app that allows users to manage their tasks through a simple interface. Productify allows friends to share their task with each other and uses positive peer pressure to encourage improvement.
+                </p>
+            </Saos>
         </div>
     </div>
 </section>
@@ -78,31 +137,17 @@
     <div class="absolute blurbox left -z-10" />
     <div class="absolute blurbox right -z-10" />
     <div class="max-w-screen-xl px-8 py-32 mx-auto z-10">
-        <h1 class="heading-2xl bold text-accent-10 w-1/3 text-center mx-auto">
-            It doesn’t have be to placeholder text.
-        </h1>
+        <Saos animation={"slide-up 1s cubic-bezier(0.55, 0, 0.1, 1) both"}>
+            <h1 class="heading-2xl bold text-accent-10 w-1/3 text-center mx-auto">
+                It doesn’t have to be placeholder text.
+            </h1>
+        </Saos>
         <div class="mx-auto flex gap-4 items-center mt-16">
-            <FeatureCard title="Make a difference" description="It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part. Blah blah blah blah" >
-                <div slot="icon">
-                    <svg width="104" height="104" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M52 39V65M64.875 52H39.125M90.625 52C90.625 57.1216 89.6259 62.193 87.6848 66.9247C85.7438 71.6564 82.8987 75.9557 79.312 79.5772C75.7253 83.1986 71.4673 86.0714 66.7811 88.0313C62.0949 89.9912 57.0723 91 52 91C46.9277 91 41.9051 89.9912 37.2189 88.0313C32.5327 86.0714 28.2747 83.1986 24.688 79.5772C21.1013 75.9557 18.2562 71.6564 16.3152 66.9247C14.3741 62.193 13.375 57.1216 13.375 52C13.375 41.6566 17.4444 31.7368 24.688 24.4228C31.9316 17.1089 41.756 13 52 13C62.244 13 72.0684 17.1089 79.312 24.4228C86.5556 31.7368 90.625 41.6566 90.625 52Z" stroke="#4BCEA4" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-            </FeatureCard>
-            <FeatureCard title="Join Productify" description="It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part. Blah blah blah blah" >
-                <div slot="icon">
-                    <svg width="104" height="104" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M52 39V65M64.875 52H39.125M90.625 52C90.625 57.1216 89.6259 62.193 87.6848 66.9247C85.7438 71.6564 82.8987 75.9557 79.312 79.5772C75.7253 83.1986 71.4673 86.0714 66.7811 88.0313C62.0949 89.9912 57.0723 91 52 91C46.9277 91 41.9051 89.9912 37.2189 88.0313C32.5327 86.0714 28.2747 83.1986 24.688 79.5772C21.1013 75.9557 18.2562 71.6564 16.3152 66.9247C14.3741 62.193 13.375 57.1216 13.375 52C13.375 41.6566 17.4444 31.7368 24.688 24.4228C31.9316 17.1089 41.756 13 52 13C62.244 13 72.0684 17.1089 79.312 24.4228C86.5556 31.7368 90.625 41.6566 90.625 52Z" stroke="#4BCEA4" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-            </FeatureCard>
-            <FeatureCard title="Challenge yourself" description="It’s easy to set a goal. Everyone does it for themselves. The hard part is adding the action part. Blah blah blah blah" >
-                <div slot="icon">
-                    <svg width="104" height="104" viewBox="0 0 104 104" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M52 39V65M64.875 52H39.125M90.625 52C90.625 57.1216 89.6259 62.193 87.6848 66.9247C85.7438 71.6564 82.8987 75.9557 79.312 79.5772C75.7253 83.1986 71.4673 86.0714 66.7811 88.0313C62.0949 89.9912 57.0723 91 52 91C46.9277 91 41.9051 89.9912 37.2189 88.0313C32.5327 86.0714 28.2747 83.1986 24.688 79.5772C21.1013 75.9557 18.2562 71.6564 16.3152 66.9247C14.3741 62.193 13.375 57.1216 13.375 52C13.375 41.6566 17.4444 31.7368 24.688 24.4228C31.9316 17.1089 41.756 13 52 13C62.244 13 72.0684 17.1089 79.312 24.4228C86.5556 31.7368 90.625 41.6566 90.625 52Z" stroke="#4BCEA4" stroke-width="6" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
-                </div>
-            </FeatureCard>
+            {#each threeCards as {title, description, icon}, i}
+                <Saos animation={`slide-up-big 1s cubic-bezier(0.55, 0, 0.1, 1) both ${i * 0.25}s`}>
+                    <FeatureCard {title} {description} {icon}  />
+                </Saos>
+            {/each}
         </div>
     </div>
 </section>
